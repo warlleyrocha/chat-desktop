dcabe51aeb3aec2f1f047ebb84ef2e1dba6aee46
warlleyrocha <warlleyrocha@icloud.com>
2026-05-15 16:44:52 -0300

    ref: deleta strings desnecessária

--- src/app/features/menu/mocks/account.mock.ts
+++ src/app/features/menu/mocks/account.mock.ts
@@ -1,13 +1,11 @@
 export interface Account {
   name: string;
   initials: string;
-  status: string;
   appVersion: string;
 }
 
 export const MOCK_ACCOUNT: Account = {
   name: 'Warley Rocha',
   initials: 'WR',
-  status: 'Emoji de Status',
   appVersion: '6.8.2',
 };
